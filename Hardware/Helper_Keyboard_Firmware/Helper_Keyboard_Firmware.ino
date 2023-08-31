ae5638fe12a24b8d4f925197419523c1c4c111f7
Ruslan Abdulin <ruslanabdulin1985@gmail.com>
2023-08-31 22:44:47 +0200

    Change button char with serial

--- Hardware/Helper_Keyboard_Firmware/Helper_Keyboard_Firmware.ino
+++ Hardware/Helper_Keyboard_Firmware/Helper_Keyboard_Firmware.ino
@@ -1,13 +1,15 @@
 #include "Keyboard.h"
+#include <ArduinoJson.h>
 // char ctrlKey = KEY_LEFT_GUI;
 
-volatile int key1 = 0;
-volatile int key2 = 0;
-volatile int key3 = 0;
+uint16_t firstModifier1 = UINT16_MAX;
+char key1 = '\0';
+
 volatile int key1PressTimer = 0;
 volatile int key1ReleaseTimer = 0;
 volatile int timerTarget = INT_MAX;
 int buttonDelay = 30; // ms
+String receivedData = "";
 
 
 void setup() {
@@ -22,16 +24,39 @@ void setup() {
 
 }
 
+
+
 void loop() {
+  if (Serial.available()) {
+    // Read input as a string
+    String inputString = Serial.readStringUntil('\n');
+  
+    // Prepare the JSON document
+    DynamicJsonDocument doc(1024);
+  
+    // Parse the JSON string
+    DeserializationError err = deserializeJson(doc, inputString);
+  
+    // Get values
+    if (doc.containsKey("key1") && doc["key1"].containsKey("value")) {
+      const char* tempStr = doc["key1"]["value"];
+      if (tempStr != nullptr && strlen(tempStr) > 0) {
+        key1 = tempStr[0];
+      }
+    }
+  
+    // Do something with values
+  }
+  
   if (millis() > timerTarget && digitalRead(2) == LOW) {
     Serial.println("Button 1 Pressed!");
-    Keyboard.press('n');
+    Keyboard.press(key1);
     timerTarget = INT_MAX;
   }
 
   if (millis() > timerTarget && digitalRead(2) == HIGH) {
     Serial.println("Button 1 Released!");
-    Keyboard.release('n');
+    Keyboard.release(key1);
     timerTarget = INT_MAX;
   }
 }
